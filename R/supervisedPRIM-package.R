8a5219e8d4ebb1dce256d64054c0f8a51c2aeff2
David Shaub <dashaub@users.noreply.github.com>
2016-08-02 06:08:08 -0600

    Update supervisedPRIM-package.R

--- R/supervisedPRIM-package.R
+++ R/supervisedPRIM-package.R
@@ -1,1 +1,23 @@
-
+#'Supervised Classification Learning and Prediction using Patient Rules Induction Method (PRIM).
+#'
+#' \tabular{ll}{
+#' Package: \tab supervisedPRIM\cr
+#' Type: \tab Package\cr
+#' Version: \tab 1.0-0\cr
+#' Date: \tab 2016-08-01\cr    
+#' License: \tab GPL-3\cr
+#' LazyLoad: \tab yes\cr
+#' LazyData: \tab yes\cr
+#' }
+#' 
+#' @author David Shaub \email{davidshaub@gmx.com}
+#' 
+#' Maintainer: David Shaub \email{davidshaub@gmx.com}
+#' @name supervisedPRIM
+#' @docType package
+#' @title supervisedPRIM
+#' @keywords package
+#' @aliases kmmisc package-kmmisc
+#'
+#' 
+##NULL
