1e01d54eee4a5cbe8e2dc9a20088ce1128f31e81
David Shaub <dashaub@users.noreply.github.com>
2016-09-28 14:20:22 -0600

    allow factors in y

--- R/supervisedPRIM.R
+++ R/supervisedPRIM.R
@@ -18,32 +18,42 @@
 #'@examples
 #'# Train a model to determine if a flower is setosa
 #'data(iris)
-#'yData <- ifelse(iris$Species == "setosa", 1L, 0L)
+#'yData <- factor(ifelse(iris$Species == "setosa", "setosa", "other"), levels = c("setosa", "other"))
 #'xData <- iris
 #'xData$Species <- NULL
 #'primModel <- supervisedPRIM(x = xData, y = yData)
 supervisedPRIM <- function(x, y, peel.alpha = 0.05, paste.alpha = 0.01,
                            mass.min = 0.05, threshold.type = 1, ...){
-   # Ensure the input is all numeric data
-   if(!all(sapply(x, class) %in% c("numeric", "integer"))){
-      stop("All columns in x must be numeric")
-   }
-   
-   # Ensure the data are binary
-   uniquey <- unique(y)
-   if(length(uniquey) != 2){
-      stop("The y data must be labeled binary 0/1 data.")
-   }
-   if(!all(unique(y) %in% c(0, 1))){
-      
-   }
-   
-   result <- prim.box(x, y, peel.alpha = peel.alpha, paste.alpha = paste.alpha,
-                  mass.min = mass.min, threshold.type = threshold.type, ...)
-   class(result) <- c("supervisedPRIM", "prim")
-   
-   
-   return(result)
+  # Ensure the input is all numeric data
+  if(!all(sapply(x, class) %in% c("numeric", "integer"))){ 
+    stop("All columns in x must be numeric")
+  }
+  
+  # Convert factor to binary
+  lev <- levels(y)
+  if(length(lev) != 2){
+    stop("Only two-class classification is supported.")
+  } else{
+   y <- ifelse(y == lev[1], 1L, 0L) 
+  }
+  
+  # Ensure the data are binary
+  uniquey <- unique(y)
+  if(length(uniquey) != 2){
+    stop("The y data must be labeled binary 0/1 data.")
+  }
+  if(!all(unique(y) %in% c(0, 1))){
+    
+  }
+  
+  result <- prim.box(x, y, peel.alpha = peel.alpha, paste.alpha = paste.alpha,
+                     mass.min = mass.min, threshold.type = threshold.type, ...)
+  # Save levels
+  result$levels <- lev
+  class(result) <- c("supervisedPRIM", "prim")
+  
+  
+  return(result)
 }
 
 #'Model Predictions
@@ -59,49 +69,63 @@ supervisedPRIM <- function(x, y, peel.alpha = 0.05, paste.alpha = 0.01,
 #'@examples
 #'# Train a model to determine if a flower is setosa
 #'data(iris)
-#'yData <- ifelse(iris$Species == "setosa", 1L, 0L)
+#'yData <- factor(ifelse(iris$Species == "setosa", "setosa", "other"), levels = c("setosa", "other"))
 #'xData <- iris
 #'xData$Species <- NULL
 #'primModel <- supervisedPRIM(x = xData, y = yData)
 #'# Predict on the original dataset
 #'predictions <- predict(primModel, newdata = xData)
 predict.supervisedPRIM <- function(object, newdata, classProb = FALSE, ...){
-   # Determine if the threshold is upper or lower
-   positive <- object$ind[1]
-   
-   # Determine label for observations outside of the boxes
-   other <- object$num.class
-   
-   # Fit the prim box on the new data
-   class(object) <- "prim"
-   primPred <- predict(object = object, newdata = newdata)
-   
-   # Determine the boxes to use
-   if(0 %in% primPred){
-     # The newdata matrix contains observations outside the range of the train set
-     other <- c(0, other)
-   }
-   inBox <- !(primPred %in% other)
-   
-   # Calculuate class probabilities using the same methodology of CART
-   if(classProb){
-     # Obtain the probabilities inside of each box
-     boxProbs <- sapply(object$y, FUN = mean)
-     
-     # Return class 0 in primPred indicates outside of training region
-     # Use mean imputation for these
-     boxProbs <- c(boxProbs, mean(unlist(object$y)))
-      return(boxProbs[primPred + 1])
-   }
-   
-
-   #classPred <- ifelse(primPred != other, positive, 1 - positive)
-   
-   shift <- ifelse(other[1] == "0", 1L, 0L)
-   if(object$ind[1 + shift ] == 1){
-      classPred <- ifelse(inBox, 1L, 0L)
-      return(classPred)
-   }
-   classPred <- ifelse(inBox, 0L, 1L)
-   return(classPred)
+  # Determine if the threshold is upper or lower
+  positive <- object$ind[1]
+  
+  # Determine label for observations outside of the boxes
+  other <- object$num.class
+  
+  # Fit the prim box on the new data
+  class(object) <- "prim"
+  primPred <- predict(object = object, newdata = newdata)
+  
+  # Determine the boxes to use
+  if(0 %in% primPred){
+    # The newdata matrix contains observations outside the range of the train set
+    other <- c(0, other)
+  }
+  inBox <- !(primPred %in% other)
+  
+  # Calculuate class probabilities using the same methodology of CART
+  if(classProb){
+    # Obtain the probabilities inside of each box
+    boxProbs <- sapply(object$y, FUN = mean)
+    
+    # Return class 0 in primPred indicates outside of training region
+    # Use mean imputation for these
+    boxProbs <- c(boxProbs, mean(unlist(object$y)))
+    return(boxProbs[primPred + 1])
+  }
+  
+  
+  #classPred <- ifelse(primPred != other, positive, 1 - positive)
+  
+  shift <- ifelse(other[1] == "0", 1L, 0L)
+  if(!shift){
+    #firstSlot <- ifelse(positive == 1, 1, 0)
+    #secondSlot <- ifelse(positive == -1, 0, 1)
+    #classPred <- ifelse(inBox, 1L, 0L)
+    classPred <- ifelse(positive, inBox, as.numeric(!inBox))
+    classPred <- factor(ifelse(classPred == 1L, object$levels[1], object$levels[2]), levels = object$levels)
+    return(classPred)
+  }
+  classPred <- ifelse(inBox, 0L, 1L)
+  #classPred <- factor(ifelse(predict(modelFit, newdata, classProb = FALSE) == 1, modelFit$levels[1], modelFit$levels[2]), levels = modelFit$levels)
+  classPred <- factor(ifelse(classPred == 1L, object$levels[1], object$levels[2]), levels = object$levels)
+  return(classPred)
+  
+  
+  # To ensure the estimate for the negative class is unbiased,
+  # use the training sample proportion to randomly assign
+  #    if(biasAdj){
+  #       
+  #    }
+  return(classPred)
 }
